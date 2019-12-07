262c330362361b58219615c423416bfcc53908d7
Jakub <gudzikoff@gmail.com>
2019-12-07 23:45:56 +0100

    Inheritance

--- venv/Include/student.py
+++
@@ -1,5 +0,0 @@
-class Student:
-    def __init__(self,firstName="",lastName="",present = False):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.present = present
